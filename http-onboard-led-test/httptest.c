70e64dda859feeaf8e1820cdebccd13f964aa828
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2023-01-25 11:20:47 +0000

    3 led blink fix

--- http-onboard-led-test/httptest.c
+++ http-onboard-led-test/httptest.c
@@ -1,15 +1,36 @@
 #include "picow_http/http.h"
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
-int
-main(void)
-{
+int main(void) {
 	struct server *srv;
 	struct server_cfg cfg;
-
-	/*
-	 * Connection to WLAN, and any other initialization for your app
-	 * here ...
-	 */
+	
+	stdio_init_all();
+    	if (cyw43_arch_init()) {
+        	printf("WiFi init failed");
+        	return -1;
+    	}
+    	
+    	
+	char ssid[] = "Ledziaki";
+	char pass[] = "12345678";
+	
+	if (cyw43_arch_init()) {
+		printf("WIFI init failed, exiting\n");
+		return -1;
+	}
+	
+	print("WIFI initialised\n");
+	
+	cyw43_arch_enable_sta_mode();
+	
+	if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
+		printf("Failed to connect\n");
+		return -1;
+	}
+	
+	print("WIFI connected")
 
 	/* Get a default configuration for the http server */
 	cfg = http_default_cfg();
