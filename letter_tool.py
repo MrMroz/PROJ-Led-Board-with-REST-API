b2c48fe98db45d039a9f1597b77189b0f5e8cce4
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2023-05-23 20:32:26 +0200

    Cyfry i drobne poprawki

--- letter_tool.py
+++ letter_tool.py
@@ -11,17 +11,13 @@ TEMPLATE = "\nuint8_t <NAME>[32][19] = {"
 THRESHOLDING = 200
 
 
-terminate = False
 if not os.path.exists(IN_DIR):
     os.makedirs(IN_DIR)
-    terminate = True
+    exit()
 
 if not os.path.exists(OUT_DIR):
     os.makedirs(OUT_DIR)
-    terminate = True
-
-if terminate:
-    exit()
+    
 
 def in_pth(img_name: str) -> str:
     return os.path.join(IN_DIR, img_name)
@@ -29,30 +25,32 @@ def in_pth(img_name: str) -> str:
 def out_pth(img_name: str) -> str:
     return os.path.join(OUT_DIR, img_name)
 
-with open(out_pth('all.txt'), mode='w', encoding='utf-8') as out_f:
-    for in_file in os.listdir(IN_DIR):
-        if not in_file.endswith(".png"):
-            continue
-        
-        name: str = in_file.replace('.png', '')
-        with open(out_pth(name + '.txt'), mode='w', encoding='utf-8') as f:
-            img = cv2.imread(in_pth(in_file), -1)
-            img.astype(np.uint8)
 
-            out = TEMPLATE.replace('<NAME>', name)
-
-            for row in img:
-                out += '\n\t\t\t\t\t\t  {'
-                for pixel in row:
-                    if pixel[3] >= THRESHOLDING:
-                        out += '0xFF, '
-                    else:
-                        out += '0x00, '
-                out = out[:-2]
-                out += '},'
+if __name__ == '__main__':
+    with open(out_pth('all.txt'), mode='w', encoding='utf-8') as out_f:
+        for in_file in os.listdir(IN_DIR):
+            if not in_file.endswith(".png"):
+                continue
             
-            out = out[:-1]
-            out += '\n\t\t\t\t\t};'
+            name: str = in_file.replace('.png', '')
+            with open(out_pth(name + '.txt'), mode='w', encoding='utf-8') as f:
+                img = cv2.imread(in_pth(in_file), -1)
+                img.astype(np.uint8)
 
-            f.write(out)
-        out_f.write(out+'\n')
+                out = TEMPLATE.replace('<NAME>', name)
+
+                for row in img:
+                    out += '\n\t\t\t\t\t\t  {'
+                    for pixel in row:
+                        if pixel[3] >= THRESHOLDING:
+                            out += '0xFF, '
+                        else:
+                            out += '0x00, '
+                    out = out[:-2]
+                    out += '},'
+                
+                out = out[:-1]
+                out += '\n\t\t\t\t\t};'
+
+                f.write(out)
+            out_f.write(out+'\n')
